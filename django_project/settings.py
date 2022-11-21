f141f5570b0823a0095a71afc931625c654af594
Mehmet Ali Ozcan <maliozcan@gmail.com>
2022-11-21 23:05:49 +0100

    Mask db password
    DB password masked.

--- django_project/settings.py
+++ django_project/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'swe573_project',
         'USER': 'root',
-        'PASSWORD': 'Mali1984!!',
+        'PASSWORD': '**********',
         'HOST': '127.0.0.1',
         'PORT': '3306',
     }
