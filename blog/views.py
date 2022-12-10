759db7220c3ed16a1ad4f7e8227d4ef04e288137
Mehmet Ali Ozcan <maliozcan@gmail.com>
2022-12-10 20:38:50 +0100

    Writing posts, bootstap and navigation bar added.
    Writing posts is now added to the app. Besides, bootstrap is used to improve the UI. Navigation bar is also added.

--- blog/views.py
+++ blog/views.py
@@ -1,25 +1,11 @@
 from django.shortcuts import render
-
-posts = [
-{
-    'author': 'Mehmet Ali Özcan',
-    'title': 'Post 1',
-    'content': 'It is the first post',
-    'date_posted': '06.12.2022'
-},
-{
-    'author': 'Gözde Bor Özcan',
-    'title': 'Post 2',
-    'content': 'It is the second post',
-    'date_posted': '06.12.2022'
-}
-]
+from .models import Post
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
 def about(request):
-    return render(request, 'blog/about.html')
+    return render(request, 'blog/about.html', {'title': 'About'})
